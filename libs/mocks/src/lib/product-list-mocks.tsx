aab31a1ed7a5086dcfc554f8c88aa8f8d9812f65
workray <oleksiishushkevych@gmail.com>
2023-11-02 17:14:32 -0400

    setting up a mocked product list

---
+++ libs/mocks/src/lib/product-list-mocks.tsx
@@ -0,0 +1,27 @@
+interface productListItem {
+  id: string;
+  title: string;
+  image: string;
+  price: number;
+}
+export const PRODUCT_LIST_MOCKS: Array<productListItem> = [
+  {
+    id: '1',
+    title: 'Apples',
+    image: '/assets/apple.jpg',
+    price: 1.99,
+  },
+  {
+    id: '2',
+    title: 'Oranges',
+    image: '/assets/orange.jpg',
+    price: 2.5,
+  },
+  {
+    id: '3',
+    title: 'Bananas',
+    image: '/assets/banana.jpg',
+    price: 0.7,
+  },
+];
+export default PRODUCT_LIST_MOCKS;
