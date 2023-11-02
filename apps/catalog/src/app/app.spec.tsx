343b920562c6459db189498b4b5bb66dd4ff09c4
workray <oleksiishushkevych@gmail.com>
2023-11-02 17:06:48 -0400

    using a shared component library

--- apps/catalog/src/app/app.spec.tsx
+++ apps/catalog/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome catalog/gi)).toBeTruthy();
+    expect(getByText(/Catalog App/gi)).toBeTruthy();
   });
 });
