5fe272c6d8ec622ee8b492bde03550cecb666a4f
workray <oleksiishushkevych@gmail.com>
2023-11-02 20:19:45 -0400

    state management with module federation

--- apps/app-shell/src/remotes.d.ts
+++ apps/app-shell/src/remotes.d.ts
@@ -1,2 +1,3 @@
 declare module 'catalog/Module';
 declare module 'checkout/Module';
+declare module 'store/Module';
--- apps/store/module-federation.config.ts
+++ apps/store/module-federation.config.ts
@@ -1,8 +1,10 @@
 import { ModuleFederationConfig } from '@nx/webpack';
 
 const config: ModuleFederationConfig = {
-  name: 'app-shell',
-  remotes: ['catalog', 'checkout', 'store'],
+  name: 'store',
+  exposes: {
+    './Module': './src/remote-entry.ts',
+  },
 };
 
 export default config;
