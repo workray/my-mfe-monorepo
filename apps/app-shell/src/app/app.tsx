43c9570db38a0eb48e1fcdb0f00c3f6bb1c88f6b
workray <oleksiishushkevych@gmail.com>
2023-11-03 04:21:11 -0400

    adding the like button to the host app

--- apps/app-shell/src/app/app.tsx
+++ apps/app-shell/src/app/app.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Button } from 'semantic-ui-react';
 import { Route, Routes } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { Header } from '@ebuy/ui';
+import useStore from 'store/Module';
+
 const Catalog = React.lazy(() => import('catalog/Module'));
 const Checkout = React.lazy(() => import('checkout/Module'));
+
 export function App() {
+  const { count, increment } = useStore();
   return (
     <React.Suspense fallback={null}>
       <Container style={{ marginTop: '5rem' }}>
         <Header />
+        <Button onClick={increment}>{count} Likes</Button>
         <Routes>
           <Route path="/" element={<Catalog />} />
           <Route path="/catalog" element={<Catalog />} />
--- apps/recommendations/src/app/app.tsx
+++ apps/recommendations/src/app/app.tsx
@@ -1,10 +1,15 @@
 import 'semantic-ui-css/semantic.min.css';
+import { Button } from 'semantic-ui-react';
+import useStore from 'store/Module';
 
 export function App() {
+  const { count, reset } = useStore();
   return (
     <div className="ui raised segment">
       <h1>Recommendations</h1>
       <p>Recommendations goes here</p>
+      <p> {count} people liked the recommendations</p>
+      <Button onClick={reset}>reset</Button>
     </div>
   );
 }
